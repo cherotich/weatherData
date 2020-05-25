7cc65417c9a69b7ac37a3f13cfb4ea3444aef463
cherotich <carolcaro60@gmail.com>
2020-05-25 23:45:41 +0300

    updated the api

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,50 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 admin.initializeApp();
 
-exports.getWeatherData = functions.https.onRequest(async (req,res)=>{
- 
-    var Weather_Data=  await admin.database().ref('/Weather_Data').once('value');
-    res.send(Weather_Data)
+const app = require("express")();
+
+app.get("/Weather_Data", async (req, res) => {
+  const arrays = [];
+  var Weather_Data = await admin
+    .database()
+    .ref("/Weather_Data")
+    .once("value")
+    .then((gotData) => {
+      
+      gotData.forEach((snapshiot) => {
+       // const temp = snapshiot.val().Temperature;
+       
+        arrays.push( temp=snapshiot.val().Temperature);
+      });
+      return arrays;
+    })
+    .then((rres) => res.send(rres))
+    .catch(error => res.send(error));
+
 });
 
-exports.getDailyWeatherData = functions.https.onRequest(async (req,res) =>{
-    var daily = await admin.database().ref('/Weather_Data').orderByChild('Time').startAt('15:51:33').endAt('15:56:10').once('value');
-    res.send(daily)
+app.get("/getDailyWeatherData", async (req, res) => {
+  await admin
+    .database()
+    .ref("/Weather_Data")
+    .orderByChild("Time")
+    .startAt("15:51:33")
+    .endAt("15:52:44")
+    .once("value", gotData);
+
+  function gotData(data) {
+    var dataweather = data.val();
+    let array1 = [];
+    array1.push(Object.values(dataweather));
+
+    console.log(array1);
+    res.send(array1);
+  }
+  function errData(err) {
+    console.log(err);
+  }
 });
+
+exports.api = functions.region("europe-west1").https.onRequest(app);
